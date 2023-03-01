c23781e84e073d231dcd06dc11e3e6da8a1d1718
BlackAlpha0 <kaveenuththara@gmail.com>
2023-03-01 13:16:55 +0530

    commit read-file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+//feature -> console-log
 console.log("Application Framfork")
+
+//Read File | feature -> read-file
+const fs = require('fs')
+fs.readFile('File.txt', 'utf8', function(err,data) {
+    if (err) throw err;
+    console.log(data);
+});
