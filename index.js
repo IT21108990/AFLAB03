39d286e82a8c033457ad0985eb0f793b180e0104
IT21108990 <IT21108990@my.sliit.lk>
2023-03-01 16:17:34 +0530

    commit write file

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ fs.readFile('File.txt', 'utf8', function(err,data) {
     if (err) throw err;
     console.log(data);
 });
+
+
+//Write to file | feature -> write-file
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+    if (err) throw err;
+    console.log('File saved!');
+});
+
+
